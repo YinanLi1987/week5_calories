3fe7b62a9af9103377bfcc363b2d0c4ed8ee5930
YinanLi1987 <t1liyi00@students.oamk.fi>
2023-01-20 16:29:29 +0200

    week 5 done

---
+++ app/src/main/java/com/example/calorie/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.calorie.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFFABA64)
+val Purple500 = Color(0xFFD94232)
+val Purple700 = Color(0xFFD643F0)
+val Teal200 = Color(0xFF3AE4FC)
